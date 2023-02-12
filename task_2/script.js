2f5ab2f7a73695f8c458b49915b1873d031ca5f1
Oliwier Kossak <oliwierkossak@wp.pl>
2023-02-12 21:29:57 +0100

    if else, arithmetic operators and console log

---
+++ task_2/script.js
@@ -0,0 +1,17 @@
+const score = 15
+let num1 = 8
+let x = 5
+let y = 10
+
+num1++;
+
+if(score % 2 == 0){
+    console.log(`parzysta`);
+}
+else{
+    console.log(`nieparzysta`);
+}
+
+console.log(num1);
+
+console.log(x*y);
